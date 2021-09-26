2682ca2627a6cc69f943e3448d15dcf0eeba02d1
Ankit Sagar <ankit.sagar.me@gmail.com>
2021-09-26 10:21:36 +0530

    static and global recursion

---
+++ Recusion/static_global.cpp
@@ -0,0 +1,40 @@
+//Static Variables in Recursion
+#include <iostream>
+#include <stdio.h>
+
+ using namespace std;
+
+int staticV(int n) {
+  static int x=0;
+  if(n>0){
+      x++;
+      return staticV(n-1)+x;
+  }
+ return 0;
+}
+
+//Global Variabels in Recursion
+int x=0;
+int global(int n) {
+    if(n>0) {
+     x++;
+     return global(n-1)+x;
+   }
+     return 0;
+}
+
+int main() {
+
+    int r;
+    r=staticV(5);
+    cout<<r<<endl;
+    r=staticV(5);
+    cout<<r<<endl;
+    r=global(5);
+    cout<<r<<endl;
+    r=global(5);
+    cout<<r<<endl;
+  return 0;
+}
+
+
