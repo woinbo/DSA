00c68e5e984e182686aa56fe4e438338eae9c070
Ankit Sagar <ankit.sagar.me@gmail.com>
2021-09-28 11:21:35 +0530

    typo fix, sum of N, nested recursion

---
+++ Recursion/Nested_Recursion.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <iostream>
+
+ using namespace std;
+
+//  Nested Recursion
+int fun(int n) {
+    if(n>100)
+        return n-10;
+    return fun(fun(n+11));
+}
+int main() {
+    int r;
+    r=fun(95);
+    cout<<r;
+    return 0;
+}
+   
