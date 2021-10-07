69be2aa98d5247168a46558e32ae84ddabfd96c3
Ankit Sagar <ankit.sagar.me@gmail.com>
2021-10-07 13:37:14 +0530

    2d arrays

---
+++ array/2d_array.cpp
@@ -0,0 +1,16 @@
+// 2D Array
+#include <stdio.h>
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int A[3][4] = {{1, 2, 3, 4}, {2, 4, 6, 8}, {1, 3, 5, 7}};
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 4; j++)
+            cout << A[i][j] << " ";
+        cout << endl;
+    }
+    return 0;
+}
