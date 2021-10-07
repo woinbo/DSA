620b9ddeedf019ef6eea81d5587d4d6875530162
Ankit Sagar <ankit.sagar.me@gmail.com>
2021-10-07 13:03:33 +0530

    array size

---
+++ array/array_size.cpp
@@ -0,0 +1,27 @@
+// Array Size
+
+#include <stdio.h>
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int *p, *q;
+    int i;
+    p = new int[10];
+    p[0] = 3;
+    p[1] = 5;
+    p[2] = 7;
+    p[3] = 9;
+    p[4] = 11;
+    q = new int[10];
+    for (i = 0; i < 5; i++)
+        q[i] = p[i];
+    free(p);
+    p = q;
+    q = NULL;
+    for (i = 0; i < 5; i++)
+        cout << p[i] << endl;
+    return 0;
+}
