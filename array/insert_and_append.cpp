2f4d456f1e1a21c376d2e6a567634fe29e871d42
Ankit Sagar <ankit.sagar.me@gmail.com>
2021-10-07 15:42:04 +0530

    Linear search

--- array/insert_and_append.cpp
+++ array/insert_and_append.cpp
@@ -19,13 +19,14 @@ class Array {
     void Display();                 // Display the Array
     void Append(int x);             // Append at the end
     void Insert(int index, int x);  // Insert at the index
+    int LinearSearch(int);
 };
 
 void Array::Display() {
     int i;
-    printf("\nElements are\n");
+    cout << "Elements are:" << endl;
     for (i = 0; i < length; i++)
-        printf("%d ", A[i]);
+        cout << A[i];
 }
 
 void Array::Append(int x) {
@@ -43,17 +44,35 @@ void Array::Insert(int index, int x) {
     }
 }
 
+void swap(int *x, int *y) {
+    int temp = *x;
+    *x = *y;
+    *y = temp;
+}
+
+int Array::LinearSearch(int key) {
+    int i;
+    for (i = 0; i < length; i++) {
+        if (key == A[i]) {
+            swap(A[i], A[0]);
+            return i;
+        }
+    }
+    return -1;
+}
+
 int main() {
     int size;
     cout << "Enter the size of array";
     cin >> size;
     Array arr1 = Array(size);
+    cout << "Enter the element to be inserted: " << endl;
     for (int i = 0; i < size; i++) {
         int x;
-        cout << "Enter the element to be inserted";
         cin >> x;
         arr1.Append(x);
     }
     arr1.Display();
+    cout << arr1.LinearSearch(2);
     return 0;
 }
